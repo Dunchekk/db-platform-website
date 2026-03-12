505f7cc577864bf3178947999615678e0bf811f7
dunchek <vasilevaevdokia875@gmail.com>
2026-03-12 09:27:33 +0300

    feat: add LayersStack.tsx

---
+++ frontend/src/features/layer-switching/LayersStack.tsx
@@ -0,0 +1,56 @@
+// src/widgets/layers-stack/ui/LayersStack.tsx
+import { LayerShell } from "@/layers/LayerShell";
+import { useLayersStore } from "@/features/layer-switching/layers.store";
+import { isBlurredLayer } from "@/features/layer-switching/get-layer-state";
+
+import InfoLayer from "@/layers/InfoLayer/InfoLayer";
+import AboutLayer from "@/layers/AboutLayer/AboutLayer";
+import ObjectsLayer from "@/layers/ObjectsLayer/ObjectsLayer";
+import CheckoutLayer from "@/layers/CheckoutLayer/CheckoutLayer";
+import DetailsLayer from "@/layers/DetailsLayer/DetailsLayer";
+
+export function LayersStack() {
+  const openedLayers = useLayersStore((state) => state.openedLayers);
+
+  const isObjectsOpen = openedLayers.includes("objects");
+  const isCheckoutOpen = openedLayers.includes("checkout");
+  const isInfoOpen = openedLayers.includes("info");
+
+  return (
+    <>
+      <AboutLayer />
+
+      <LayerShell
+        isOpen={isObjectsOpen}
+        isBlurred={isBlurredLayer(openedLayers, "objects")}
+        zIndex={20}
+      >
+        <ObjectsLayer />
+      </LayerShell>
+
+      <LayerShell
+        isOpen={isObjectsOpen}
+        isBlurred={isBlurredLayer(openedLayers, "details")}
+        zIndex={30}
+      >
+        <DetailsLayer />
+      </LayerShell>
+
+      <LayerShell
+        isOpen={isInfoOpen}
+        isBlurred={isBlurredLayer(openedLayers, "info")}
+        zIndex={30}
+      >
+        <InfoLayer />
+      </LayerShell>
+
+      <LayerShell
+        isOpen={isCheckoutOpen}
+        isBlurred={isBlurredLayer(openedLayers, "checkout")}
+        zIndex={40}
+      >
+        <CheckoutLayer />
+      </LayerShell>
+    </>
+  );
+}
