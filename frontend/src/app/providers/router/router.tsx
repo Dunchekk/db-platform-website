d9df5ad6c1e4254ea62ee6ef29c80f7705a1d663
dunchek <vasilevaevdokia875@gmail.com>
2026-03-12 09:19:03 +0300

    feat: add appRouter

---
+++ frontend/src/app/providers/router/router.tsx
@@ -0,0 +1,27 @@
+import App from "@/app/App";
+import { createBrowserRouter } from "react-router";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/checkout",
+    element: <App />,
+  },
+  {
+    path: "/info",
+    element: <App />,
+  },
+  {
+    path: "/object/:id",
+    element: <App />,
+  },
+  {
+    path: "/object/:id/checkout",
+    element: <App />,
+  },
+]);
+
+export default appRouter;
