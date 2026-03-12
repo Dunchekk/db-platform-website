863233fc2a5c890be818328b5bc5375fb68545fb
dunchek <vasilevaevdokia875@gmail.com>
2026-03-12 11:03:51 +0300

    build: track /config/build

---
+++ frontend/config/build/buildLoaders.ts
@@ -0,0 +1,61 @@
+import { ModuleOptions } from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshTypeScript from "react-refresh-typescript";
+import { BuildOptions } from "./types/types";
+
+export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
+  const isDev = options.mode === "development";
+
+  const styleOrExtract = isDev ? "style-loader" : MiniCssExtractPlugin.loader;
+
+  const accetLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: "asset/resource",
+  };
+
+  const svgrLoader = {
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+  };
+
+  const cssModuleLoader = {
+    test: /\.module\.css$/i,
+    use: [
+      styleOrExtract,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+            namedExport: false,
+          },
+        },
+      },
+    ],
+  };
+
+  const cssLoader = {
+    test: /\.css$/i,
+    exclude: /\.module\.css$/i,
+    use: [styleOrExtract, "css-loader"],
+  };
+
+  const tsLoader = {
+    exclude: /node_modules/,
+    test: /\.tsx?$/,
+    use: [
+      {
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+          getCustomTransformers: () => ({
+            before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
+          }),
+        },
+      },
+    ],
+  };
+
+  return [accetLoader, cssModuleLoader, cssLoader, tsLoader, svgrLoader];
+}
---
+++ frontend/config/build/buildPlugins.ts
@@ -0,0 +1,45 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin, { Configuration } from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/types";
+// import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import { DefinePlugin } from "webpack";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ReactRefrashWebpackPluguin from "@pmmmwh/react-refresh-webpack-plugin";
+import path from "path";
+
+export function buildPlugins(params: BuildOptions): Configuration["plugins"] {
+  const isDev = params.mode === "development";
+  const isProd = params.mode === "production";
+
+  const plugins: Configuration["plugins"] = [
+    new HtmlWebpackPlugin({
+      template: params.paths.html,
+      favicon: path.resolve(params.paths.public, "favicon.svg"),
+    }),
+    new DefinePlugin({
+      __PLATFORM__: JSON.stringify(params.platform),
+    }), // подменяет глобальные переменные на значения которые мы задаем при сборке
+  ];
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: "css/[name].[contenthash:8].css",
+        chunkFilename: "css/[name].[contenthash:8].css",
+      })
+    );
+  }
+
+  if (isDev) {
+    plugins.push(
+      new ForkTsCheckerWebpackPlugin(), // выносим проверку типов в отдельный процесс
+      new ReactRefrashWebpackPluguin()
+    );
+  }
+
+  if (params.analyzer) {
+    // plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return plugins;
+}
