7a1b7161f5911207b650f5e5521222fa6ec82e87
dunchek <vasilevaevdokia875@gmail.com>
2026-03-12 09:19:49 +0300

    refactor: refactor index.ts with appRouter

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import AppShall from "./app/AppShall";
+import { RouterProvider } from "react-router";
+import appRouter from "./app/providers/router/router";
 
 const root = document.getElementById("root");
 
@@ -8,11 +8,6 @@ if (!root) {
   throw new Error("root not found");
 }
 
-const router = createBrowserRouter([
-  {
-    path: "*",
-    element: <AppShall />,
-  },
-]);
+const router = appRouter;
 
 ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
