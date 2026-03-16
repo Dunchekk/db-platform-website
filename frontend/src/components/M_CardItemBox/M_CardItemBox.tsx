6b31b13d4d8274a1c206e5771032107983fb15e2
dunchek <vasilevaevdokia875@gmail.com>
2026-03-16 17:49:49 +0300

    feat: add M_CardItemBox for CardLayer

---
+++ frontend/src/components/M_CardItemBox/M_CardItemBox.tsx
@@ -0,0 +1,37 @@
+import cls from "@/components/M_CardItemBox/M_CardItemBox.module.css";
+
+import React from "react";
+
+import objects from "@/mocks/objects.json";
+import { DbObject } from "@/shared/types/object";
+
+const object: DbObject = objects[0];
+
+type Props = {
+  object: DbObject;
+  cardInfo: number;
+};
+
+const M_CardItemBox = ({ object, cardInfo }: Props) => {
+  return (
+    <div className={cls.wrapper}>
+      <div className={cls.img}>
+        <img src={object.img} alt="cardImage" />
+      </div>
+      <div className={cls.info}>
+        <div className={cls.left}>
+          <div className={cls.innerleft}>
+            <span>
+              {object.name} ({object.id})
+            </span>
+            <span>m</span>
+          </div>
+          <div>— {cardInfo} +</div>
+        </div>
+        <div className={cls.prise}>{object.prise} ₽</div>
+      </div>
+    </div>
+  );
+};
+
+export default M_CardItemBox;
