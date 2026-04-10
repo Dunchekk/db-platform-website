0f77ca38d36fb8d3a00f6a9e3fe9a3e88f528fe5
dunchek <vasilevaevdokia875@gmail.com>
2026-04-10 07:28:12 +0300

    feat: create ApiError class & bind it to errorHendler middleware

---
+++ backend/src/middleware/ErrorHandlingMiddleware.ts
@@ -0,0 +1,11 @@
+import ApiError from "../error/ApiError";
+import type { ErrorRequestHandler } from "express";
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (err instanceof ApiError) {
+    return res.status(err.status).json({ message: err.message });
+  }
+  return res.status(500).json({ message: "unpredict error" });
+};
+
+export default errorHandler;
