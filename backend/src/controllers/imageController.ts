147b71932cc2dfbb550889fdf2adf9ab68b4f6db
dunchek <vasilevaevdokia875@gmail.com>
2026-04-11 12:07:28 +0300

    feat: add all ImageController functions (upd, upl, del, get, getBy)

--- backend/src/controllers/imageController.ts
+++ backend/src/controllers/imageController.ts
@@ -1,11 +1,50 @@
 import type { NextFunction, Request, Response } from "express";
 import { prisma } from "../db";
 import ApiError from "../error/ApiError";
+import { parseIdParam } from "../helpers/parseIdParam";
 
 class ImageController {
-  async getAllFiles(req: Request, res: Response, next: NextFunction) {}
+  async getAllFiles(req: Request, res: Response, next: NextFunction) {
+    // api/images/... потом сделать для получения всех картинок конкретного объекта
 
-  // api/images/uploads...
+    try {
+      const images = await prisma.itemImage.findMany();
+      res.json(images);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        next(ApiError.badRequest(err.message));
+        return;
+      }
+
+      next(ApiError.badRequest("Unknown error"));
+    }
+  }
+
+  async getItemFiles(req: Request, res: Response, next: NextFunction) {
+    // GET /api/images/:id
+
+    try {
+      const id = parseIdParam(req.params.id);
+
+      const images = await prisma.itemImage.findMany({
+        where: {
+          itemId: id,
+        },
+        orderBy: { position: "asc" }, // ascending = по возрастанию
+      });
+
+      res.json(images);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        next(ApiError.badRequest(err.message));
+        return;
+      }
+
+      next(ApiError.badRequest("Unknown error"));
+    }
+  }
+
+  // POST /api/images/...
   async uploadFile(req: Request, res: Response, next: NextFunction) {
     if (!req.file) {
       return res.status(400).json({ message: "File was not uploaded" }); // есть req.file -> multer файл поймал
@@ -14,10 +53,7 @@ class ImageController {
     try {
       const url = `/uploads/${req.file.filename}`;
 
-      if (!Number(req.params.id)) {
-        throw ApiError.badRequest("Params do not have any id!");
-      }
-      const iId = Number(req.params.id);
+      const iId = parseIdParam(req.params.id);
 
       const image = await prisma.itemImage.create({
         data: {
@@ -28,25 +64,110 @@ class ImageController {
       });
 
       res.status(201).json(image);
-    } catch (err) {
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        next(ApiError.badRequest(err.message));
+        return;
+      }
+
+      next(ApiError.badRequest("Unknown error"));
+    }
+  }
+
+  async deleteFile(req: Request, res: Response, next: NextFunction) {
+    // DELETE api/images/:id/:imageId
+    try {
+      const imageId = parseIdParam(req.params.imageId);
+
+      // проверка на сущестовование
+      const image = await prisma.itemImage.findUnique({
+        where: { id: imageId },
+      });
+
+      if (!image) {
+        throw ApiError.notFound("Image not found by this id");
+      }
+
+      await prisma.itemImage.delete({ where: { id: imageId } });
+
+      res.status(200).json({ message: "Image deleted" });
+    } catch (err: unknown) {
       if (err instanceof ApiError) {
         next(err);
         return;
       }
-      next(new Error("Unknown error"));
+
+      if (err instanceof Error) {
+        next(ApiError.badRequest(err.message));
+        return;
+      }
+
+      next(ApiError.badRequest("Unknown error"));
     }
   }
 
-  async bindFileTo(req: Request, res: Response) {}
+  async changeFile(req: Request, res: Response, next: NextFunction) {
+    // PATCH api/images/:id/:imageId
 
-  async deleteFile(req: Request, res: Response) {}
+    // с фронта:
+    // {
+    //     "id": 4,
+    //     "url": "/static/uploads/3_19875276_1645700735499975_4334392123731064015_n.png",
+    //     "itemId": 3,
+    //     "position": 0
+    // }
+    try {
+      const imageId = parseIdParam(req.params.imageId);
+      const itemId = parseIdParam(req.params.id);
+      const updateImage = req.body;
 
-  async changeFile(req: Request, res: Response) {}
+      // проверка на сущестовование
+      const image = await prisma.itemImage.findUnique({
+        where: { id: imageId },
+      });
+
+      if (!image) {
+        throw ApiError.notFound("Image not found by this id");
+      }
+
+      // проверка на соответсвие
+      if (
+        !updateImage ||
+        Number(updateImage.id) !== imageId ||
+        Number(updateImage.itemId) !== itemId
+      ) {
+        next(ApiError.badRequest("Request do not have required body"));
+        return;
+      }
+
+      const newImage = await prisma.itemImage.update({
+        data: {
+          position: updateImage.position,
+        },
+        where: {
+          id: imageId,
+        },
+      });
+
+      return res.json(newImage);
+    } catch (err: unknown) {
+      if (err instanceof ApiError) {
+        next(err);
+        return;
+      }
+
+      if (err instanceof Error) {
+        next(ApiError.badRequest(err.message));
+        return;
+      }
+
+      next(ApiError.badRequest("Unknown error"));
+    }
+  }
 }
-
 export const imageController = new ImageController();
 
-//Это уже финальный route handler, то есть контроллер.
+// Это уже финальный route handler, то есть контроллер.
 // Он не ловит файл “с нуля”, а работает с уже обработанным request.
 
 // Его задача:
--- backend/src/routes/imageRouter.ts
+++ backend/src/routes/imageRouter.ts
@@ -3,15 +3,15 @@ import { imageController } from "../controllers/imageController";
 import { uploadMiddleware } from "../middleware/UploadMiddleware";
 const imageRouter = Router();
 
-imageRouter.get("/about-images", imageController.getAllFiles); // для получения всех картинок
+imageRouter.get("/", imageController.getAllFiles); // для получения всех картинок
 
-imageRouter.post("/uploads/:id", uploadMiddleware, imageController.uploadFile); // Загрузить файл на сервер. Обычно возвращает url, fileId или path.
+imageRouter.get("/:id", imageController.getItemFiles); // для получения картинок конкретного объекта
 
-imageRouter.post("/objects/:id/images", imageController.bindFileTo); // Привязать картинку к объекту.
+imageRouter.post("/:id", uploadMiddleware, imageController.uploadFile); // Загрузить файл на сервер + привязать к айди объекта. возвращаем объект картинки
 
-imageRouter.delete("/objects/:id/images/:imageId", imageController.deleteFile); // Удалить картинку у объекта.
+imageRouter.delete("/:id/:imageId", imageController.deleteFile); // Удалить картинку у объекта.
 
-imageRouter.patch("/objects/:id/images/:imageId", imageController.changeFile); // Например, поменять порядок, сделать главной и т.п.
+imageRouter.patch("/:id/:imageId", imageController.changeFile); // Например, поменять порядок, сделать главной и т.п.
 
 // api/images/...
 
