cc4717fdec6698581326c083b288fa4e94c58219
dunchek <vasilevaevdokia875@gmail.com>
2026-04-13 07:52:08 +0300

    feat: add & bind AuthController (login, checkAuth)

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,19 +1,50 @@
 import type { Request, Response, NextFunction } from "express";
 import ApiError from "../error/ApiError";
+import argon2 from "argon2";
+import "dotenv/config";
+import { SignJWT } from "jose";
+
+const adminEmail = process.env.ADMIN_EMAIL!;
+const adminPasswordHash = process.env.ADMIN_PASSWORD_HASH!;
+const jwtSecret = new TextEncoder().encode(process.env.JWT_SECRET!);
 
 class AuthController {
-  async registration(req: Request, res: Response) {}
+  async login(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
 
-  async login(req: Request, res: Response) {}
+    if (!email || !password) {
+      return next(ApiError.badRequest("Email and password are requied"));
+    }
 
-  async logOut(req: Request, res: Response) {}
+    if (email !== adminEmail) {
+      return next(ApiError.badRequest("Email/password is incorrect"));
+    }
+
+    const isValidPassword = await argon2.verify(adminPasswordHash, password);
+
+    if (!isValidPassword) {
+      return next(ApiError.badRequest("Email/password is incorrect"));
+    }
+
+    const token = await new SignJWT({ role: "ADMIN" })
+      .setProtectedHeader({ alg: "HS256" })
+      .setSubject("admin")
+      .setIssuedAt()
+      .setExpirationTime("2h")
+      .sign(jwtSecret);
+
+    return res.json({ token });
+  }
 
   async checkAuth(req: Request, res: Response, next: NextFunction) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest("No id attribute"));
-    }
-    res.json(id);
+    const token = await new SignJWT({ role: "ADMIN" })
+      .setProtectedHeader({ alg: "HS256" })
+      .setSubject("admin")
+      .setIssuedAt()
+      .setExpirationTime("2h")
+      .sign(jwtSecret);
+
+    return res.json({ token });
   }
 }
 
--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { authController } from "../controllers/authController";
+import authMiddleware from "../middleware/AuthMiddleware";
 const authRouter = Router();
 
-authRouter.post("/registration", authController.registration); // авторизация -- Отправить логин и пароль.
 authRouter.post("/login", authController.login); // авторизация -- Отправить логин и пароль.
-authRouter.post("/logout", authController.logOut); // выйти из сессии
-authRouter.get("/session", authController.checkAuth); // проверка авторизован или нет по jwt токену, есть ли активная сессия при старте приложения.
+authRouter.get("/session", authMiddleware, authController.checkAuth); // проверка авторизован или нет по jwt токену, есть ли активная сессия при старте приложения.
 
 // api/auth...
 
