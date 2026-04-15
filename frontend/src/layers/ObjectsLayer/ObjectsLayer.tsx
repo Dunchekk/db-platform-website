d34b0b11df341ef505f39d0510ab4dfbec27c9a8
dunchek <vasilevaevdokia875@gmail.com>
2026-04-15 11:04:54 +0300

    feat: upd logic of ObjectsLyaer to new object types

--- frontend/src/layers/ObjectsLayer/ObjectsLayer.tsx
+++ frontend/src/layers/ObjectsLayer/ObjectsLayer.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import cls from "@/layers/ObjectsLayer/ObjectsLayer.module.css";
 import M_itemCard from "@/components/M_itemCard/M_itemCard";
 import { DbObject } from "@/shared/types/object.types";
-import { useObjects } from "@/shared/objects/objects.context";
+import { useObjects } from "@/features/objects/objects.store";
 import { useLayersStore } from "@/features/layer-switching/layers.store";
 import Q_InfoButtons from "@/components/Q_InfoButtons/Q_InfoButtons";
 import { useAuth } from "@/features/auth/auth.store";
 
-const PROTOTYPE_ONLY_FIRST_OBJECT_CLICKABLE = true;
-
 const ObjectsLayer = () => {
-  const { objects, isLoading, error } = useObjects();
   const openLayer = useLayersStore((state) => state.openLayer);
   const setActiveObjectId = useLayersStore((state) => state.setActiveObjectId);
+  const objects = useObjects((state) => state.objects);
 
   const wrapperClasses =
     objects.length === 1
@@ -28,23 +26,14 @@ const ObjectsLayer = () => {
     <div className={cls.main}>
       <div className={cls.wrapper}>
         <div className={cls.wrapper2}>
-          {isLoading && <div>Loading...</div>}
-          {error && <div>{error}</div>}
-          {objects.map((obj: DbObject, index) => {
-            const isClickable =
-              !PROTOTYPE_ONLY_FIRST_OBJECT_CLICKABLE || index === 0;
+          {objects.map((obj: DbObject) => {
             return (
               <div
                 key={obj.id}
-                onClick={
-                  isClickable
-                    ? () => {
-                        setActiveObjectId(String(obj.id));
-                        openLayer("details");
-                      }
-                    : undefined
-                }
-                style={{ cursor: isClickable ? "pointer" : "default" }}
+                onClick={() => {
+                  setActiveObjectId(String(obj.id));
+                  openLayer("details");
+                }}
               >
                 <M_itemCard addClasses={wrapperClasses} object={obj} />
               </div>
