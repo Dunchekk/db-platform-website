51225e3efaef347490f25e9a0334e3950282a0bc
dunchek <vasilevaevdokia875@gmail.com>
2026-04-15 11:08:12 +0300

    chore: comment new mock data (in useObjects)

--- frontend/src/features/objects/objects.store.ts
+++ frontend/src/features/objects/objects.store.ts
@@ -5,54 +5,54 @@ import { create } from "zustand";
 
 export const useObjects = create<ObjectsState>((set) => ({
   objects: [
-    {
-      id: 3,
-      createdAt: "2026-04-10T10:25:11.000Z",
-      updatedAt: "2026-04-10T10:25:11.000Z",
-      name: "Leather Bag",
-      price: 12900,
-      order: 1,
-      images: [
-        {
-          id: 7,
-          url: "/uploads/3_bag-front.png",
-          itemId: 3,
-          position: 0,
-        },
-        {
-          id: 8,
-          url: "/uploads/3_bag-side.png",
-          itemId: 3,
-          position: 1,
-        },
-      ],
-      points: [
-        {
-          id: 11,
-          itemId: 3,
-          point: "натуральная кожа",
-        },
-        {
-          id: 12,
-          itemId: 3,
-          point: "ручная работа",
-        },
-      ],
-      info: [
-        {
-          id: 21,
-          itemId: 3,
-          title: "Размер",
-          description: "30x20 см",
-        },
-        {
-          id: 22,
-          itemId: 3,
-          title: "Фурнитура",
-          description: "металл",
-        },
-      ],
-    },
+    // {
+    //   id: 3,
+    //   createdAt: "2026-04-10T10:25:11.000Z",
+    //   updatedAt: "2026-04-10T10:25:11.000Z",
+    //   name: "Leather Bag",
+    //   price: 12900,
+    //   order: 1,
+    //   images: [
+    //     {
+    //       id: 7,
+    //       url: "/uploads/3_bag-front.png",
+    //       itemId: 3,
+    //       position: 0,
+    //     },
+    //     {
+    //       id: 8,
+    //       url: "/uploads/3_bag-side.png",
+    //       itemId: 3,
+    //       position: 1,
+    //     },
+    //   ],
+    //   points: [
+    //     {
+    //       id: 11,
+    //       itemId: 3,
+    //       point: "натуральная кожа",
+    //     },
+    //     {
+    //       id: 12,
+    //       itemId: 3,
+    //       point: "ручная работа",
+    //     },
+    //   ],
+    //   info: [
+    //     {
+    //       id: 21,
+    //       itemId: 3,
+    //       title: "Размер",
+    //       description: "30x20 см",
+    //     },
+    //     {
+    //       id: 22,
+    //       itemId: 3,
+    //       title: "Фурнитура",
+    //       description: "металл",
+    //     },
+    //   ],
+    // },
   ],
   setObjects: (objects: DbObject[]) => set({ objects }),
 }));
