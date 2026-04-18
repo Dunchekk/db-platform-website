f76855d7542f9057e6beaebf5464f933c6c96ea6
dunchek <vasilevaevdokia875@gmail.com>
2026-04-18 15:27:10 +0300

    feat: add new css styles to O_CheckoutCustomerFields component

--- frontend/src/components/organisms/O_CheckoutCustomerFields/O_CheckoutCustomerFields.tsx
+++ frontend/src/components/organisms/O_CheckoutCustomerFields/O_CheckoutCustomerFields.tsx
@@ -11,62 +11,70 @@ const O_CheckoutCustomerFields = ({
 }: ComponentPropsWithoutRef<"div">) => {
   return (
     <div className={className} {...props}>
-      <p>ваши данные:</p>
-      <M_Input
-        name="firstName"
-        id={"имя"}
-        required
-        placeholder="имя*"
-      ></M_Input>
-      <M_Input
-        name="lastName"
-        id={"фамилия"}
-        required
-        placeholder="фамилия*"
-      ></M_Input>
-      <M_Input
-        name="patronymic"
-        id={"отчество"}
-        placeholder="отчество"
-      ></M_Input>
-      <M_Input
-        name="email"
-        type="email"
-        id={"почта"}
-        required
-        placeholder="почта*"
-      ></M_Input>
-      <M_Input
-        name="phone"
-        id={"телефон"}
-        required
-        placeholder="телефон*"
-      ></M_Input>
-      <M_Input
-        id={"телеграмм"}
-        name="telegram"
-        required
-        placeholder="телеграмм (@example)"
-      ></M_Input>
-      <div className={""}>
-        <M_InputCheckbox required className={cls.checkbox} id="policy" />
-        <label className={""} htmlFor="policy">
-          Я принимаю условия
-          <Link to="/info/offer" onClick={(event) => event.stopPropagation()}>
-            Публичной оферты
-          </Link>
-          и соглашаюсь с
-          <Link to="/info/privacy" onClick={(event) => event.stopPropagation()}>
-            Политикой обработки персональных данных
-          </Link>
-          *
-        </label>
+      <div>
+        <p>ваши данные:</p>
+        <M_Input
+          name="firstName"
+          id={"имя"}
+          required
+          placeholder="имя*"
+        ></M_Input>
+        <M_Input
+          name="lastName"
+          id={"фамилия"}
+          required
+          placeholder="фамилия*"
+        ></M_Input>
+        <M_Input
+          name="patronymic"
+          id={"отчество"}
+          placeholder="отчество"
+        ></M_Input>
+        <M_Input
+          name="email"
+          type="email"
+          id={"почта"}
+          required
+          placeholder="почта*"
+        ></M_Input>
+        <M_Input
+          name="phone"
+          id={"телефон"}
+          required
+          placeholder="телефон*"
+        ></M_Input>
+        <M_Input
+          id={"телеграмм"}
+          name="telegram"
+          required
+          placeholder="телеграмм (@example)"
+        ></M_Input>
       </div>
-      <div className={""}>
-        <M_InputCheckbox required className={cls.checkbox} id="coolness" />
-        <label className={""} htmlFor="coolness">
-          Я очень крут*
-        </label>
+      <div className={cls.checkboxes}>
+        <M_InputCheckbox required className={cls.checkbox} id="policy">
+          <label htmlFor="policy">
+            Я принимаю условия{" "}
+            <Link
+              to="/info/offer"
+              className={cls.links}
+              onClick={(event) => event.stopPropagation()}
+            >
+              Публичной оферты
+            </Link>{" "}
+            и соглашаюсь с{" "}
+            <Link
+              to="/info/privacy"
+              className={cls.links}
+              onClick={(event) => event.stopPropagation()}
+            >
+              Политикой обработки персональных данных
+            </Link>
+            *
+          </label>
+        </M_InputCheckbox>
+        <M_InputCheckbox required className={cls.checkbox} id="coolness">
+          <label htmlFor="coolness">Я очень крут*</label>
+        </M_InputCheckbox>
       </div>
     </div>
   );
