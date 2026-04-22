b6625ec50b151ae374c9b4bfebcae1ec668cf797
dunchek <vasilevaevdokia875@gmail.com>
2026-04-22 15:34:33 +0300

    feat: add isAuthChecked & isObjectsReady check in BootLoaderOnReady.tsx

--- frontend/src/app/providers/boot-loader/BootLoaderOnReady.tsx
+++ frontend/src/app/providers/boot-loader/BootLoaderOnReady.tsx
@@ -1,21 +1,24 @@
 import { useEffect, useRef } from "react";
 
 import { hideBootLoader } from "@/app/providers/boot-loader/bootLoader";
-import { useObjects } from "@/shared/objects/objects.context";
+import { useAuth } from "@/features/auth/auth.store";
+import { useObjects } from "@/features/objects/objects.store";
 
 const BootLoaderOnReady = (): null => {
-  const { isLoading } = useObjects();
-  const hasHiddenRef = useRef(false);
+  const hasHiddenRef = useRef(false); // если loader уже скрывали, второй раз не пытаться;
+  const isAuthChecked = useAuth((state) => state.isAuthChecked);
+  const isObjectsReady = useObjects((state) => state.isObjectsReady);
 
   useEffect(() => {
-    if (hasHiddenRef.current) return;
-    if (isLoading) return;
-
+    if (hasHiddenRef.current || !isAuthChecked || !isObjectsReady) return;
     hasHiddenRef.current = true;
     hideBootLoader();
-  }, [isLoading]);
+  }, [isAuthChecked, isObjectsReady]);
 
   return null;
 };
+
+// boot loader существует в index.html до старта приложения
+// один раз скрывается и удаляетсяж
 
 export default BootLoaderOnReady;
