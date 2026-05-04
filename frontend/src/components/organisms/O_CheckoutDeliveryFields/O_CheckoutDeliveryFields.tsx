5d0c319f028a1a78e1d4f92092d8cee222e10369
dunchek <vasilevaevdokia875@gmail.com>
2026-05-04 14:25:46 +0300

    feat: add list of cdek cities to checkout layer (O_CheckoutDeliveryFields.tsx)

--- frontend/src/components/organisms/O_CheckoutDeliveryFields/O_CheckoutDeliveryFields.tsx
+++ frontend/src/components/organisms/O_CheckoutDeliveryFields/O_CheckoutDeliveryFields.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import M_InputCheckbox from "../../molecules/M_InputCheckbox/M_InputCheckbox";
 import M_Input from "../../molecules/M_Input/M_Input";
 import type { ComponentPropsWithoutRef } from "react";
 import cls from "@/components/organisms/O_CheckoutDeliveryFields/O_CheckoutDeliveryFields.module.css";
 import A_Button from "../../atoms/A_Button/A_Button";
+import { getCities } from "@/shared/api/cdek";
+import { CdekSuggestedCity } from "@/shared/types/cdek.types";
 
 type Props = {
   isCartEmpty: boolean;
@@ -16,10 +18,67 @@ const O_CheckoutDeliveryFields = ({
   showToast,
   ...props
 }: Props) => {
+  const [isCitiesOpen, setIsCitiesOpen] = useState<boolean>(false);
+  const [cities, setCities] = useState<CdekSuggestedCity[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_selectedCity, setSelectedCity] = useState<CdekSuggestedCity | null>(
+    null
+  );
+  const [query, setQuery] = useState<string>("");
+
+  const handleCityInputChange = async (
+    e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>
+  ) => {
+    const value = e.target.value.trim();
+    setQuery(e.target.value);
+
+    if (value.length < 2) {
+      setCities([]);
+      return;
+    }
+
+    let newCities: CdekSuggestedCity[] = [];
+    try {
+      newCities = await getCities(value);
+    } catch (e) {
+      showToast(`Ошибка при загрузке городов: ${e.message}`, "error");
+    }
+    setCities(newCities);
+  };
+
+  const handleClick = (city: CdekSuggestedCity) => {
+    setSelectedCity(city);
+    setQuery(city.label);
+    setIsCitiesOpen(false);
+  };
+
   return (
     <div className={className} {...props}>
       <p>доставка:</p>
-      <M_Input placeholder="город*" />
+      <M_Input
+        placeholder="город*"
+        onChange={handleCityInputChange}
+        value={query}
+        onFocus={() => setIsCitiesOpen(true)}
+      />
+
+      {isCitiesOpen && cities.length !== 0 && (
+        <div className={cls.list}>
+          <ul className={cls.ul}>
+            {cities.map((city) => {
+              return (
+                <li
+                  key={city.code}
+                  onClick={() => handleClick(city)}
+                  className={cls.li}
+                >
+                  {city.label}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
 
       <div>
         <M_InputCheckbox id="cdek1">
