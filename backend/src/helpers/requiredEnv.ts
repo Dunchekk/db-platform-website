b4d0d3401f67a594267403ace24bcf89020c8a90
dunchek <vasilevaevdokia875@gmail.com>
2026-05-04 10:48:06 +0300

    feat: add requiredEnv function for cdekConfig

---
+++ backend/src/helpers/requiredEnv.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export function requiredEnv(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Missing env: ${name}`);
+  }
+  return value;
+}
