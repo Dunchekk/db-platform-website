eee99d226f59897b56da37bc200f6fb0e32a8fe1
dunchek <vasilevaevdokia875@gmail.com>
2026-05-04 11:20:54 +0300

    chore: add CdekSuggestedCity types

--- backend/src/types/cdek.types.ts
+++ backend/src/types/cdek.types.ts
@@ -4,3 +4,17 @@ export type CdekTokenResponse = {
   expires_in: number;
   scope?: string;
 };
+
+export type CdekSuggestedCityDto = {
+  city_uuid: string;
+  code: number;
+  full_name: string;
+  country_code: string;
+};
+
+export type NormalizedCdekSuggestedCityDto = {
+  uuid: string;
+  code: number;
+  label: string;
+  countryCode: string;
+}[];
