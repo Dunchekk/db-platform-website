87e2e179af3ef082245858ebc7c395ea87e10ae9
dunchek <vasilevaevdokia875@gmail.com>
2026-05-04 11:23:39 +0300

    feat: add & bind cdekRouter

---
+++ backend/src/routes/cdekRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { cdekController } from "../controllers/cdekController";
+
+const cdekRouter = Router();
+
+cdekRouter.get("/cities", cdekController.getCitiesByParams); // для получения всех городов по запросу
+
+export default cdekRouter;
