851983442b88ed7cac1aaa97902022e72bbd1f22
dunchek <vasilevaevdokia875@gmail.com>
2026-05-05 07:08:20 +0300

    feat: add getOfficesByParams endpoint (cdekRouter.ts)

--- backend/src/routes/cdekRouter.ts
+++ backend/src/routes/cdekRouter.ts
@@ -4,5 +4,6 @@ import { cdekController } from "../controllers/cdekController";
 const cdekRouter = Router();
 
 cdekRouter.get("/cities", cdekController.getCitiesByParams); // для получения всех городов по запросу
+cdekRouter.get("/delivery-points", cdekController.getOfficesByParams); // для получения всех городов по запросу
 
 export default cdekRouter;
