b700917a1f591b575c58a009a63f013399296e5b
dunchek <vasilevaevdokia875@gmail.com>
2026-05-31 10:34:52 +0300

    feat: bind fetching cdek delivery prices to roating (backend/src/routes/cdekRouter.ts)

--- backend/src/routes/cdekRouter.ts
+++ backend/src/routes/cdekRouter.ts
@@ -5,5 +5,6 @@ const cdekRouter = Router();
 
 cdekRouter.get("/cities", cdekController.getCitiesByParams); // для получения всех городов по запросу
 cdekRouter.get("/delivery-points", cdekController.getOfficesByParams); // для получения всех городов по запросу
+cdekRouter.get("/delivery-price", cdekController.getDeliveryPriceByCity); // для получения цены по городу и весу
 
 export default cdekRouter;
