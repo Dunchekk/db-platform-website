a443854536ec79e731315d7c552f981a30874b15
dunchek <vasilevaevdokia875@gmail.com>
2026-05-31 10:30:24 +0300

    feat: add getDeliveryPrice api function (frontend)

--- frontend/src/shared/api/cdek.ts
+++ frontend/src/shared/api/cdek.ts
@@ -1,6 +1,11 @@
 import { $host } from ".";
 import { CdekOffice, CdekSuggestedCity } from "../types/cdek.types";
-import { CDEK_GET_CITIES_URL, CDEK_GET_OFFICES_URL } from "./endpoints";
+import { NormalizedDeliveryInfo } from "../types/checkout.types";
+import {
+  CDEK_GET_CITIES_URL,
+  CDEK_GET_DELIVERY_PRICE_URL,
+  CDEK_GET_OFFICES_URL,
+} from "./endpoints";
 
 export const getCities = async (
   query: string
@@ -12,6 +17,22 @@ export const getCities = async (
 export const getOffices = async (city_code: number): Promise<CdekOffice[]> => {
   const response = await $host.get(
     CDEK_GET_OFFICES_URL + `?city_code=${city_code}`
+  );
+  return response;
+};
+
+export const BASE_WEIGHT = 500; // в граммах
+
+export const getDeliveryPrice = async (
+  city_code: number,
+  weight: number
+): Promise<NormalizedDeliveryInfo> => {
+  const params = new URLSearchParams({
+    city_code: String(city_code),
+    weight: String(weight),
+  });
+  const response = await $host.get(
+    `${CDEK_GET_DELIVERY_PRICE_URL}?${params.toString()}`
   );
   return response;
 };
