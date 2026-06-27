78b0c8e2b1237845dd19ff888d84bcf07a6f1ab6
dunchek <vasilevaevdokia875@gmail.com>
2026-06-27 11:37:32 +0300

    chore: add types for creating order (checkout.types.ts, backend)

--- backend/src/types/checkout.types.ts
+++ backend/src/types/checkout.types.ts
@@ -1,3 +1,5 @@
+import { CdekOfficeFromFront, CdekSuggestedCityFromFront } from "./cdek.types";
+
 export type ReqOrderBody = {
   firstName: string;
   lastName: string;
@@ -10,6 +12,22 @@ export type ReqOrderBody = {
   subtotal: number;
   total: number;
   items: ReqOrderItem[];
+  office: CdekOfficeFromFront;
+  city: CdekSuggestedCityFromFront;
+};
+
+export type PreparedOrderItem = {
+  itemId: number;
+  title: string;
+  price: number;
+  quantity: number;
+  total: number;
+};
+
+export type PreparedOrder = {
+  subtotal: number;
+  orderItemsData: PreparedOrderItem[];
+  totalQuantity: number;
 };
 
 export type ReqOrderItem = {
