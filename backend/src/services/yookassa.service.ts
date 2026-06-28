822ba63287f37b4957ca5ff9ac21ca85f7a63acd
dunchek <vasilevaevdokia875@gmail.com>
2026-06-28 20:30:15 +0300

    feat: add yookassa.service.ts & CreatePayload function

---
+++ backend/src/services/yookassa.service.ts
@@ -0,0 +1,28 @@
+import { ICreatePayment } from "@a2seven/yoo-checkout"; // OR const { YooCheckout } = require('@a2seven/yoo-checkout');
+import { Order } from "@prisma/client";
+import "dotenv";
+
+export async function CreatePayload(
+  order: Order,
+  orderId: number,
+  paymentId: number
+) {
+  return {
+    amount: {
+      value: String(order.total) + ".00",
+      currency: "RUB",
+    },
+    payment_method_data: {
+      type: "bank_card",
+    },
+    confirmation: {
+      type: "redirect",
+      return_url: process.env.FRONTEND_RETURN_URL,
+    },
+    metadata: {
+      time: Date.now().toString(),
+      orderId, // нормальный
+      paymentId, // нормальный
+    },
+  } as ICreatePayment;
+}
