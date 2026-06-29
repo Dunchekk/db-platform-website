df652046c842d00749a5d12b3c2733205eb72748
dunchek <vasilevaevdokia875@gmail.com>
2026-06-29 11:06:01 +0300

    chore: add new variants in mapYooKassaStatus handler

--- backend/src/helpers/mapYooKassaStatus.ts
+++ backend/src/helpers/mapYooKassaStatus.ts
@@ -1,15 +1,20 @@
 import { IPaymentStatus } from "@a2seven/yoo-checkout";
 import { PaymentStatus } from "@prisma/client";
 
-export const mapYooKassaStatus = (s: IPaymentStatus): PaymentStatus => {
+export const mapYooKassaStatus = (
+  s: IPaymentStatus | string
+): PaymentStatus => {
   switch (s) {
     case "waiting_for_capture":
+    case "payment.waiting_for_capture":
       return "PENDING";
     case "pending":
       return "PENDING";
     case "succeeded":
+    case "payment.succeeded":
       return "SUCCEEDED";
     case "canceled":
+    case "payment.canceled":
       return "CANCELED";
     default:
       return "PENDING";
