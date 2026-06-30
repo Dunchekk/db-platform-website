89864eecb6c651e991f47a7aed9252ff5aaf3d32
dunchek <vasilevaevdokia875@gmail.com>
2026-06-30 17:38:34 +0300

    feat: move YouKassa init to yookassa.service.ts

--- backend/src/services/yookassa.service.ts
+++ backend/src/services/yookassa.service.ts
@@ -1,6 +1,16 @@
 import { ICreatePayment } from "@a2seven/yoo-checkout"; // OR const { YooCheckout } = require('@a2seven/yoo-checkout');
 import { Order } from "@prisma/client";
+import { YooCheckout } from "@a2seven/yoo-checkout"; // OR const { YooCheckout } = require('@a2seven/yoo-checkout');
+
 import "dotenv";
+
+const YOUKASSA_SECRET_KEY = process.env.YOUKASSA_SECRET_KEY as string;
+const SHOP_ID = process.env.SHOP_ID as string;
+
+export const YouKassa = new YooCheckout({
+  shopId: SHOP_ID,
+  secretKey: YOUKASSA_SECRET_KEY,
+});
 
 export async function CreatePayload(
   order: Order,
@@ -22,8 +32,8 @@ export async function CreatePayload(
     },
     metadata: {
       time: Date.now().toString(),
-      orderId, // нормальный
-      paymentId, // нормальный
+      orderId,
+      paymentId,
     },
   } as ICreatePayment;
 }
