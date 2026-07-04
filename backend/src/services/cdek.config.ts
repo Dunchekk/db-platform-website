9e88bdddc27d4e6f59f1efef413d961f0cf2a4fb
dunchek <vasilevaevdokia875@gmail.com>
2026-07-04 15:34:17 +0300

    refactor: refactor cdek.service.ts -> cdek.service.ts (orchestration), cdek.api.ts (api), cdek.config.ts

---
+++ backend/src/services/cdek.config.ts
@@ -0,0 +1,23 @@
+import { requiredEnv } from "../helpers/requiredEnv";
+
+export const cdekConfig = {
+  baseUrl: requiredEnv("CDEK_BASE_URL"),
+  clientId: requiredEnv("CDEK_CLIENT_ID"),
+  clientSecret: requiredEnv("CDEK_CLIENT_SECRET"),
+  countryCode: requiredEnv("CDEK_COUNTRY_CODE"),
+};
+
+export const cdekOrderProperties = {
+  weightMin: null, // Минимальный вес (в кг.), принимаемый в ПВЗ (> WeightMin)
+  weightMax: "0", // Максимальный вес (в кг.), принимаемый в ПВЗ (<=WeightMax)
+  length: "40", // величина посылок
+  width: "30",
+  height: "8",
+  baseWeight: "350",
+  fromCityCode: 44,
+  tariffCode: 136,
+  name: "Дубовицкий Иван Максимович",
+  inn: "0000000000000000",
+  phone: "24567820957",
+  shipmentPoint: "",
+};
