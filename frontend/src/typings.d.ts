95adf72f9b4ca461f1ca617789e358a421067628
dunchek <vasilevaevdokia875@gmail.com>
2026-07-27 21:53:35 +0300

    chore: remove unused svgr loader

--- frontend/src/typings.d.ts
+++ frontend/src/typings.d.ts
@@ -7,10 +7,6 @@ declare module "*.png";
 declare module "*.jpeg";
 declare module "*.jpg";
 declare module "*.gif";
-declare module "*.svg" {
-  import React from "react";
-  const SVG: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default SVG;
-}
+declare module "*.svg";
 
 declare const __PLATFORM__: "desktop" | "mobile";
