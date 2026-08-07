6e14791a346d9b80a95c91fb5966e1139a9f67ff
dunchek <vasilevaevdokia875@gmail.com>
2026-08-07 21:57:13 +0300

    refactor: move info docs tsx to frontend/src/shared/legal/

---
+++ frontend/src/shared/legal/current.ts
@@ -0,0 +1,5 @@
+export const CURRENT_OFFER_VERSION = "1.1.1";
+export const CURRENT_PERSONAL_DATA_CONSENT_VERSION = "1.1.1";
+
+export { default as CurrentOffer } from "./offer/Offer_v1_1_1";
+export { default as CurrentPersonalDataConsent } from "./personal-data-consent/PersonalDataConsent_v1_1_1";
